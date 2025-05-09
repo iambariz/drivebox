34aa68bb49b8d1e44d839f0b1c3fa28029732ab1
iambariz <david.barkanyi96@gmail.com>
2025-05-09 07:41:05 +0100

    Added options window

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sys
 import threading
 from pynput import keyboard
-from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction, QMessageBox
+from PyQt5.QtWidgets import (
+    QApplication, QSystemTrayIcon, QMenu, QAction, QMessageBox, QDialog,
+    QVBoxLayout, QLabel, QPushButton, QLineEdit
+)
 from PyQt5.QtGui import QIcon
 from screenshot import take_screenshot
 from auth import get_gdrive_service
@@ -9,11 +12,39 @@ from upload import upload_file_to_drivebox
 import pyperclip
 import os
 
+# --- Options Window ---
+class OptionsWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("DriveBox Options")
+        self.setFixedSize(300, 200)
+        layout = QVBoxLayout()
+
+        self.status_label = QLabel("Not signed in")
+        self.login_button = QPushButton("Sign in to Google Drive")
+        self.login_button.clicked.connect(self.login_to_gdrive)
+
+        self.hotkey_label = QLabel("Hotkey (not implemented):")
+        self.hotkey_input = QLineEdit("Ctrl+Alt+X")
+
+        layout.addWidget(self.status_label)
+        layout.addWidget(self.login_button)
+        layout.addWidget(self.hotkey_label)
+        layout.addWidget(self.hotkey_input)
+        self.setLayout(layout)
+
+    def login_to_gdrive(self):
+        service = get_gdrive_service()
+        if service:
+            self.status_label.setText("Signed in to Google Drive!")
+        else:
+            self.status_label.setText("Sign in failed.")
+
+# --- Main App Logic ---
 def login_to_gdrive():
     service = get_gdrive_service()
     if service:
         print("Google Drive authentication successful.")
-        # Optional: Show a message box
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Information)
         msg.setText("Google Drive authentication successful!")
@@ -37,13 +68,11 @@ def take_and_upload():
     else:
         print("Screenshot failed.")
 
-
 def on_hotkey():
     print("Hotkey pressed!")
     take_and_upload()
 
 def start_hotkey_listener():
-    # Todo: Change hotkey / Give user option to change hotkey
     COMBO = {keyboard.Key.ctrl_l, keyboard.Key.alt_l, keyboard.KeyCode(char='x')}
     current = set()
 
@@ -60,7 +89,6 @@ def start_hotkey_listener():
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
 
-
 def main():
     # Start hotkey listener in a separate thread
     hotkey_thread = threading.Thread(target=start_hotkey_listener, daemon=True)
@@ -69,12 +97,20 @@ def main():
     app = QApplication(sys.argv)
     tray_icon = QSystemTrayIcon(QIcon("icon.png"), app)
     menu = QMenu()
+
+    # Create the options window
+    options_window = OptionsWindow()
+    options_action = QAction("Options")
+    options_action.triggered.connect(options_window.show)
+
     login_action = QAction("Login to Google Drive")
     login_action.triggered.connect(login_to_gdrive)
     screenshot_action = QAction("Take Screenshot")
     screenshot_action.triggered.connect(take_and_upload)
     exit_action = QAction("Exit")
     exit_action.triggered.connect(app.quit)
+
+    menu.addAction(options_action)
     menu.addAction(login_action)
     menu.addAction(screenshot_action)
     menu.addAction(exit_action)
